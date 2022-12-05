06f27e608323ae0ae30f3e4ca1535c865c6fdd90
Lucas Rodrigues <lucasrodrigues062@gmail.com>
2022-12-05 13:19:11 -0300

    Criado classe retorno iqvia

---
+++ server/src/main/kotlin/br/symbiosys/solucoes/cronospharma/cronospharma/entidades/iqvia/ItemRetornoNotaIqvia.kt
@@ -0,0 +1,15 @@
+package br.symbiosys.solucoes.cronospharma.cronospharma.entidades.iqvia
+
+class ItemRetornoNotaIqvia {
+
+    var codigoEAN: String? = null
+    var codigoProdutoDistribuidor: String? = null
+    var quantidade: String? = null
+    var tipoEmbalagem: String? = null
+    var preco: String? = null
+    var descontoComercial: Double = 0.0
+    var valorRepasse: Double = 0.0
+    var repasse: Double = 0.0
+    var valorUnitario: Double = 0.0
+    var fracionamento: Int = 0
+}
---
+++ server/src/main/kotlin/br/symbiosys/solucoes/cronospharma/cronospharma/entidades/iqvia/RetornoNotaIqvia.kt
@@ -0,0 +1,40 @@
+package br.symbiosys.solucoes.cronospharma.cronospharma.entidades.iqvia
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class RetornoNotaIqvia {
+    // Cabecalho
+    val dataGeracaoArquivo = LocalDateTime.now()
+    var cnpjDistribuidor: String? = null
+    var numeroPedidoOl: String? = null
+    var numeroPedidoCliente: String? = null
+
+    // dados nota fiscal
+    var dataSaidaMercadoria: LocalDateTime? = null
+    var dataEmissaoNF: LocalDate? = null
+    var cnpjCliente: String? = null
+    var numeroNotaFiscal: String? = null
+    var serieNF: String? = null
+    var vencimentoFatura: String? = null
+
+    // totais nota fiscal
+    var valorFrete: Double = 0.0
+    var valorSeguro: Double = 0.0
+    var valorDespesas: Double = 0.0
+    var valorTotalProdutos: Double = 0.0
+    var valorTotalNF: Double = 0.0
+    var valorIPI: Double = 0.0
+
+    //Impostos da nota fical
+    var baseCalculoIcms: Double = 0.0
+    var valorIcms: Double = 0.0
+    var baseCalculoSubstituicaoTributaria: Double = 0.0
+    var valorIcmsSubstituicaoTributaria: Double = 0.0
+
+    // Itens da NF
+    var itens: List<ItemRetornoNotaIqvia> = mutableListOf()
+
+    // fim do arquivo
+    var quatidaItensNotaFiscal: Int = 0
+}
