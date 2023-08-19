3555592c76d2d721b6a473b75a501da065ceb84f
Lucas Rodrigues <lucasrodrigues062@gmail.com>
2023-08-19 16:24:16 -0300

    Criado triggers e tabela necessaria para iniciar

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,4 +1,208 @@
 
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'ZProdutosCompl') AND type in (N'U'))
+BEGIN
+CREATE TABLE ZProdutosCompl
+(
+    sym_ativo VARCHAR(1),
+    sym_usaFTP VARCHAR(1),
+    sym_url VARCHAR(255),
+    sym_login VARCHAR(255),
+    sym_senha VARCHAR(255),
+    sym_dir_ped_ftp VARCHAR(255),
+    sym_dir_ret_ftp VARCHAR(255),
+    sym_dir_ped_local VARCHAR(255),
+    sym_dir_ret_local VARCHAR(255),
+    sym_dir_imp_local VARCHAR(255),
+    CodTd_sym_tipo VARCHAR(255),
+    sym_dir_est_ftp varchar(255),
+    sym_dir_est_local VARCHAR(255),
+    sym_dir_preco_ftp VARCHAR(255),
+    sym_dir_preco_local VARCHAR(255),
+    IdProduto INT
+);
+
+END;
+
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_ativo'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_ativo VARCHAR(1);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_usaFTP'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_usaFTP VARCHAR(1);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_url'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_url VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_login'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_login VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_senha'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_senha VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_dir_ped_ftp'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_dir_ped_ftp VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_dir_ret_ftp'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_dir_ret_ftp VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_dir_ped_local'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_dir_ped_local VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_dir_ret_local'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_dir_ret_local VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_dir_imp_local'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_dir_imp_local VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'CodTd_sym_tipo'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD CodTd_sym_tipo VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_dir_est_ftp'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_dir_est_ftp VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_dir_est_local'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_dir_est_local VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_dir_preco_ftp'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_dir_preco_ftp VARCHAR(255);
+
+END;
+IF NOT EXISTS (
+    SELECT *
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'ZProdutosCompl'
+    AND COLUMN_NAME = 'sym_dir_preco_local'
+)
+BEGIN
+ALTER TABLE ZProdutosCompl
+    ADD sym_dir_preco_local VARCHAR(255);
+
+END;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 -- TRIGGER CLIFOR
 IF  OBJECT_ID('AuditTrigger_Cli_For', 'TR') IS  NULL
 BEGIN
@@ -378,6 +582,7 @@ EXEC dbo.sp_executesql @statement = N'
 	
 	END;'
 END;
+
 -- TRIGGER CPR
 IF  OBJECT_ID('AuditTrigger_CPR', 'TR') IS  NULL
 BEGIN
@@ -416,5 +621,77 @@ EXEC dbo.sp_executesql @statement = N'
 
 	END;'
 END;
+
+-- TRIGGER Movimento
+
+IF  OBJECT_ID('AuditTrigger_Movimento', 'TR') IS  NULL
+BEGIN
+EXEC dbo.sp_executesql @statement = N'	CREATE TRIGGER dbo.AuditTrigger_Movimento
+	ON dbo.Movimento
+	AFTER DELETE, INSERT, UPDATE
+	AS
+	BEGIN
+	    SET NOCOUNT ON;
+	   	IF EXISTS (SELECT 1 FROM inserted  WHERE IdPedidoPalm IS NOT NULL)
+	   		BEGIN
+				DECLARE @Operation NVARCHAR(10), @ID VARCHAR(20);
+
+		        IF EXISTS (SELECT * FROM deleted)
+		        BEGIN
+		            IF EXISTS (SELECT * FROM inserted)
+		                SET @Operation = ''UPDATE'';
+		            ELSE
+		                SET @Operation = ''DELETE'';
+		        END
+		        ELSE
+		            SET @Operation = ''INSERT'';
+				SELECT @ID = ISNULL((SELECT IdMov FROM deleted), (SELECT IdMov FROM inserted) )
+		        DECLARE @OldRecord NVARCHAR(MAX) = (SELECT  *   FROM deleted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER);
+		        DECLARE @NewRecord NVARCHAR(MAX) = (SELECT   *  FROM inserted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER);
+
+		        INSERT INTO dbo.zsym_eventos (data_evento, id_registro, new_register_as_json, old_register_as_json, tabela, tipo, usuario)
+		        VALUES (GETDATE(), @ID, @NewRecord, @OldRecord, ''Movimento'', @Operation, CURRENT_USER);
+
+	   		END
+
+
+	END;'
+
+END;
+-- TRIGGER Produto
+IF  OBJECT_ID('AuditTrigger_Produto', 'TR') IS  NULL
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE TRIGGER dbo.AuditTrigger_Produto
+	ON dbo.Produtos
+	AFTER DELETE, INSERT, UPDATE
+	AS
+	BEGIN
+	    SET NOCOUNT ON;
+	   		BEGIN
+				DECLARE @Operation NVARCHAR(10), @ID VARCHAR(20);
+
+		        IF EXISTS (SELECT * FROM deleted)
+		        BEGIN
+		            IF EXISTS (SELECT * FROM inserted)
+		                SET @Operation = ''UPDATE'';
+		            ELSE
+		                SET @Operation = ''DELETE'';
+		        END
+		        ELSE
+		            SET @Operation = ''INSERT'';
+				SELECT @ID = ISNULL((SELECT IdProduto FROM deleted), (SELECT IdProduto FROM inserted) )
+		        DECLARE @OldRecord NVARCHAR(MAX) = (SELECT  *   FROM deleted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER);
+		        DECLARE @NewRecord NVARCHAR(MAX) = (SELECT   *  FROM inserted FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER);
+
+		        INSERT INTO dbo.zsym_eventos (data_evento, id_registro, new_register_as_json, old_register_as_json, tabela, tipo, usuario)
+		        VALUES (GETDATE(), @ID, @NewRecord, @OldRecord, ''Produtos'', @Operation, CURRENT_USER);
+
+	   		END
+
+
+	END;'
+
+END;
+
 
 
